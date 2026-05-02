db25040b866344ad9c842d3d4816a2f677ae6838
FLAMA100 <ncheatia@gmail.com>
2026-05-02 06:59:43 -0400

    Add CarMake CarModel models and get_cars endpoint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
+from .populate import initiate
 
 logger = logging.getLogger(__name__)
 
@@ -40,3 +42,14 @@ def registration(request):
         user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         login(request, user)
         return JsonResponse({"userName": username, "status": "Authenticated"})
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
